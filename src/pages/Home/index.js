f5567e0419782ff4b52a5a7b9b2b0b23665c169e
GT <FTowerT@outlook.com>
2021-10-01 11:13:17 +0300

    Added pagination

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Select, TextField, MenuItem } from '@mui/material';
+import { Select, TextField, MenuItem, Pagination, PaginationItem } from '@mui/material';
 import _ from 'lodash';
 
 import { fetchAPI } from 'utils';
@@ -10,7 +10,7 @@ import Styles from './styles.module.css';
 
 const Home = () => {
     const [leaderboard, setLeaderboard] = useState({});
-    const [filters, setFilters] = useState({page: 1, query: "", region: ""});
+    const [filters, setFilters] = useState({page: 1, pageSize: 25, query: "", region: ""});
 
     useEffect(() => {
         fetchAPI('GET', '/leaderboard', filters)
@@ -22,31 +22,43 @@ const Home = () => {
     
     return (
         <div className={Styles.mainCont}>
-            <div className={Styles.inputCont} >
-                <TextField
-                    className={Styles.inputField}
-                    id="filled-basic"
-                    label="Search..."
-                    variant="filled"
-                    value={ filters.query }
-                    onChange={e => setFilters({...filters, query: e.target.value})}
-                />
-                <Select
-                    className={Styles.inputSelect}
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    label="Region"
-                    value={filters.region}
-                    onChange={e => setFilters({...filters, region: e.target.value})}
-                >
-                    {
-                        _.map(availableRegions, (region, index) => {
-                            return <MenuItem key={index} value={region}>{region}</MenuItem>
-                        })
-                    }
-                    <MenuItem value={""}>All regions</MenuItem>
-                </Select>
+            <div className={Styles.controlsCont}>
+                <div className={Styles.inputCont} >
+                    <TextField
+                        className={Styles.inputField}
+                        id="filled-basic"
+                        label="Search..."
+                        variant="filled"
+                        value={ filters.query }
+                        onChange={e => setFilters({...filters, query: e.target.value})}
+                    />
+                    <Select
+                        className={Styles.inputSelect}
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="Region"
+                        value={filters.region}
+                        onChange={e => setFilters({...filters, region: e.target.value})}
+                    >
+                        {
+                            _.map(availableRegions, (region, index) => {
+                                return <MenuItem key={index} value={region}>{region}</MenuItem>
+                            })
+                        }
+                        <MenuItem value={""}>All regions</MenuItem>
+                    </Select>
+                </div>
+                <div className={Styles.paginationCont}>
+                    <Pagination
+                        value={filters.page}
+                        count={Math.ceil(_.get(leaderboard, 'stats.count', 0)/filters.pageSize)}
+                        shape="rounded"
+                        variant="outlined"
+                        onChange={(e, value) => setFilters({...filters, page: value})}
+                    />
+                </div>
             </div>
+
             {
                 _.map(leaderboard.leaderboard, (leader) => {
                     return <LeaderBlock key={leader.id} leader={leader}/>
